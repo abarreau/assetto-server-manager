ff26a4f29b35ee46f08775bcd38a6faa8a5dfa33
Anthony Barreau <anthony.barreau@itk.fr>
2023-07-02 14:11:48 +0200

    Updating deps

--- race_manager.go
+++ race_manager.go
@@ -17,10 +17,10 @@ import (
 	"github.com/JustaPenguin/assetto-server-manager/pkg/when"
 
 	"4d63.com/tz"
-	"github.com/etcd-io/bbolt"
 	"github.com/go-chi/chi"
 	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
+	"go.etcd.io/bbolt"
 )
 
 var ErrCustomRaceNotFound = errors.New("servermanager: custom race not found")
